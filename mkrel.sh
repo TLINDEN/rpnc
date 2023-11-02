ef8a98c81f2d8ae71fec580fc6d9b920c3b2b779
Thomas von Dein <tom@vondein.org>
2023-11-02 10:34:40 +0100

    fix tool name

--- mkrel.sh
+++ mkrel.sh
@@ -46,7 +46,7 @@ for D in $DIST; do
     GOOS=${os} GOARCH=${arch} go build -tags osusergo,netgo -ldflags "-extldflags=-static" -o ${binfile}
     mkdir -p ${tardir}
     cp ${binfile} README.md LICENSE ${tardir}/
-    echo 'tool = tablizer
+    echo 'tool = rpn
 PREFIX = /usr/local
 UID    = root
 GID    = 0
