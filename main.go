c92762439cbf9ea5e33ee10d0db51827d6b46d0f
Thomas von Dein <tom@vondein.org>
2023-11-08 19:01:08 +0100

    bump version

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ import (
 	lua "github.com/yuin/gopher-lua"
 )
 
-const VERSION string = "2.0.5"
+const VERSION string = "2.0.6"
 
 const Usage string = `This is rpn, a reverse polish notation calculator cli.
 
