6094f480f1939bbc35273e4469d15a9813665557
Thomas von Dein <tom@vondein.org>
2025-08-08 12:30:32 +0200

    fix linter

--- main_test.go
+++ main_test.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"os"
 	"testing"
 
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
 func TestMain(m *testing.M) {
-	os.Exit(testscript.RunMain(m, map[string]func() int{
-		"testrpn": Main,
-	}))
+	testscript.Main(m, map[string]func(){
+		"rpn": main,
+	})
 }
 
 func TestRpn(t *testing.T) {
